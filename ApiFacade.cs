efa0f5bf0b23edfcd67b7671a24fdc1379c5e408
Kornelije Sajler <kornelije.sajler@qualtrak.com>
2015-08-12 10:58:49 +0200

    Changed ninject.xml to search to proper Integration dll and to have proper namespace for IApiFacade. Updated ApiFacade to have all methods async and implemented new methods for getting recordings. Added hack to store static list of List<RecordingUser> when is sent because, when getting Recordings by its ids, without this static fields there is no way to know what recorder user id is used so it is like cacache, and gets recorders user id from this static field.

--- ApiFacade.cs
+++ ApiFacade.cs
@@ -1,19 +1,20 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Configuration;
 using Qualtrak.Coach.Integration.Core.Contracts;
 using Qualtrak.Coach.Integration.Core.DTO;
 
 namespace QATestRecorder
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Net;
-    using System.Net.Http;
-    using System.Web.Configuration;
-
     public class ApiFacade : IApiFacade
     {
         private string _defaultFile = null;
+        private static List<RecordingUser> Recordings = new List<RecordingUser>();  
 
         public ApiFacade()
         {
@@ -23,51 +24,11 @@ namespace QATestRecorder
             }
         }
 
-        public Task<List<RecorderUserInfo>> GetUsersAsync(string tenantCode, string username = null, string password = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<RecordingInfo>> GetRecordingsAsync(int limit, string tenantCode, string userId, List<SearchCriteria> searchCriteria, string username = null,
-            string password = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<RecordingUser>> GetRecordingsForUsersAsync(int limit, string tenantCode, List<string> userIds, List<SearchCriteria> searchCriteria, string username = null,
-            string password = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<List<RecordingInfo>> GetRecordingsForRecordingIdsAsync(List<string> recordingIds, string tenantCode, string username = null,
-            string password = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<string> GetRecordingUrlAsync(string recordingId, string recordingOriginalUrl, string username = null,
-            string password = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task PostEvaluationScoreAsync(string tenantCode, string evaluationId, string headlineScore, string extraScore,
-            string userId, string recordingId, string username = null, string password = null)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Stream> GetStreamAsync(string url)
-        {
-            throw new NotImplementedException();
-        }
-
-        public List<RecorderUserInfo> GetUsers(string tenantCode, string username, string password)
+        public async Task<List<RecorderUserInfo>> GetUsersAsync(string tenantCode, string username = null, string password = null)
         {
             var list = new List<RecorderUserInfo>();
-          
-            for(var i=0;i<1000; i++) 
+
+            for (var i = 0; i < 1000; i++)
             {
                 RecorderUserInfo user = new RecorderUserInfo
                 {
@@ -82,16 +43,17 @@ namespace QATestRecorder
 
                 list.Add(user);
             }
-           
-            return list;
+
+            return await Task.FromResult(list);
         }
 
-        public List<RecordingInfo> GetRecordings(int limit, string tenantCode, string userId, List<SearchCriteria> critera, string username, string password)
+        public async Task<List<RecordingInfo>> GetRecordingsAsync(int limit, string tenantCode, string userId, List<SearchCriteria> searchCriteria, string username = null,
+            string password = null)
         {
             int recordingsProcessedSoFar = 0;
             var list = new List<RecordingInfo>();
 
-            for (var i = 0; i < 1000; i++) 
+            for (var i = 0; i < 1000; i++)
             {
                 RecordingInfo recoding = new RecordingInfo
                 {
@@ -104,45 +66,75 @@ namespace QATestRecorder
                 };
 
                 list.Add(recoding);
-                recordingsProcessedSoFar ++;
+                recordingsProcessedSoFar++;
 
                 if (recordingsProcessedSoFar > limit) break;
             }
 
-            return list;
+            return await Task.FromResult(list);
         }
 
-        public List<RecordingInfo> GetRecordingsForUsers(int limit, string tenantCode, List<string> userIds, List<SearchCriteria> searchCriteria, string username,
-            string password)
+        public async Task<List<RecordingUser>> GetRecordingsForUsersAsync(int limit, string tenantCode, List<string> userIds, List<SearchCriteria> searchCriteria, string username = null,
+            string password = null)
         {
-            var result = new List<RecordingInfo>();
+            var result = new List<RecordingUser>();
 
             foreach (var userId in userIds)
             {
-                result.AddRange(this.GetRecordings(limit, tenantCode, userId, searchCriteria, username, password));
+                List<RecordingInfo> recordings = await this.GetRecordingsAsync(limit, tenantCode, userId, searchCriteria, username, password);
+                List<RecordingUser> recordingUsers = recordings.Select(x => new RecordingUser { RecordingId = x.RecordingID, RecorderUserId = x.RecorderUserID }).ToList();
+                result.AddRange(recordingUsers);
+                Recordings.AddRange(recordingUsers);
+                recordings.Clear();
+                recordingUsers.Clear();
             }
 
             return result;
         }
 
-
-        public string GetRecordingUrl(string recordingId, string recordingOriginalUrl, string username, string password)
+        public async Task<List<RecordingInfo>> GetRecordingsForRecordingIdsAsync(List<string> recordingIds, string tenantCode, string username = null,
+            string password = null)
         {
-            return recordingOriginalUrl;
+            var list = new List<RecordingInfo>();
+
+            foreach (var recordingId in recordingIds)
+            {
+                string userId = Recordings.Where(x => x.RecordingId == recordingId).Select(x => x.RecorderUserId).FirstOrDefault();
+
+                var recoding = new RecordingInfo
+                {
+                    TenantCode = int.Parse(tenantCode),
+                    RecordingID = recordingId,
+                    RecorderUserID = userId,
+                    RecordingDate = DateTime.Now.AddDays(-1),
+                    Metadata = "{ \"metadata\" : [{\"label\" : \"Claim No\", \"value\" : \"123456\", \"field\" : \"claim_no\", \"type\" : \"number\"},{\"label\" : \"Caller Id\", \"value\" : \"004401232312311\", \"field\" : \"caller_id\", \"type\" : \"number\" }, {\"label\" : \"Account No\", \"value\" : \"12121231314AB\", \"field\" : \"account_no\", \"type\" : \"string\" }, {\"label\" : \"A really long label description\", \"value\" : \"A really long piece of call metadata information 1234567890 1234567890\", \"field\" : \"notes\", \"type\" : \"string\" }] }",
+                    RecordingFileName = _defaultFile ?? "a.wmv"
+                };
+
+                list.Add(recoding);
+            }
+
+            return await Task.FromResult(list);
+       }
+
+        public async Task<string> GetRecordingUrlAsync(string recordingId, string recordingOriginalUrl, string username = null,
+            string password = null)
+        {
+            return await Task.FromResult(recordingOriginalUrl);
         }
 
-        public void PostEvaluationScore(string tenantCode, string username, string password, string evaluationId,
-            string headlineScore, string extraScore, string userId, string recordingId)
+        public async Task PostEvaluationScoreAsync(string tenantCode, string evaluationId, string headlineScore, string extraScore,
+            string userId, string recordingId, string username = null, string password = null)
         {
-            
+            throw new NotImplementedException();
         }
 
-        public Stream GetStream(string url)
+        public async Task<Stream> GetStreamAsync(string url)
         {
             Stream stream = new MemoryStream(1000000 * 10);
 
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
-            using (var client = new HttpClient(){BaseAddress = new Uri(url, UriKind.Absolute)})
+            using (var client = new HttpClient() { BaseAddress = new Uri(url, UriKind.Absolute) })
             {
                 try
                 {
@@ -155,7 +147,8 @@ namespace QATestRecorder
                 }
             }
 
-            return stream;
+            return await Task.FromResult(stream);
         }
+
     }
 }
