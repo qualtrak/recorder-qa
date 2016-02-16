4a1c68954dff88b918223b52a75aa7ecadad0922
Garrard Kitchen <garrard.kitchen@qualtrak.com>
2016-02-16 19:26:43 +0000

    Changes made to assist QA in their testing:  - We can now get back different media types based on userId (RecorderUserId)  - If you pass in a userId containing any of these ("wmv", "wav", "mp3", "mp4", "avi", "mov") it will return a filename with the appropriate file type  - Each search adds to _agentRecordingsLookup list. All items are purged if a day old.  - We are now returning a Guid instead of an int for Id. Integer values were causing issue with rejected and removed media limited key range

--- RecorderApiFacade.cs
+++ RecorderApiFacade.cs
@@ -15,6 +15,27 @@ namespace QATestRecorder
     public class RecorderApiFacade : IRecorderApiFacade
     {
         private readonly string _defaultFile;
+        private static List<AgentRecordingLookup> _agentRecordingsLookup = new List<AgentRecordingLookup>();
+
+        #region Private classes
+
+        private class AgentRecordingLookup
+        {
+            public string AgentId { get; set; }
+            public string RecordingId { get; set; }
+            public string Filename { get; set; }
+            public DateTime Created { get; private set; }
+
+            public AgentRecordingLookup(string agentId, string recordingId, string filename)
+            {
+                AgentId = agentId;
+                RecordingId = recordingId;
+                Filename = filename;
+                Created = DateTime.Now;
+            }
+        }
+
+        #endregion
 
         public RecorderApiFacade()
         {
@@ -46,22 +67,28 @@ namespace QATestRecorder
             return await Task.FromResult(list);
         }
 
+
+
         public async Task<IEnumerable<Media>> GetMediaForUserAsync(string userId, MediaForUserArgs args, DataConnectorProperties properties)
         {
             int recordingsProcessedSoFar = 0;
             var list = new List<Media>();
+
+            PurgeOldRecordings();
+
 
             for (var i = 0; i < 1000; i++)
             {
                 var media = new Media
                 {
                     RecorderUserId = userId,
-                    Id = i.ToString(),
+                    Id = Guid.NewGuid().ToString(), // i.ToString(),
                     Date = DateTime.Now.AddDays(-1),
                     Metadata = "{ \"metadata\" : [{\"label\" : \"Claim No\", \"value\" : \"123456\", \"field\" : \"claim_no\", \"type\" : \"number\"},{\"label\" : \"Caller Id\", \"value\" : \"004401232312311\", \"field\" : \"caller_id\", \"type\" : \"number\" }, {\"label\" : \"Account No\", \"value\" : \"12121231314AB\", \"field\" : \"account_no\", \"type\" : \"string\" }, {\"label\" : \"A really long label description\", \"value\" : \"A really long piece of call metadata information 1234567890 1234567890\", \"field\" : \"notes\", \"type\" : \"string\" }] }",
-                    FileName = _defaultFile ?? "a.wmv"
+                    FileName = ConvertAgentIdToMediaFile(userId)
                 };
 
+                _agentRecordingsLookup.Add(new AgentRecordingLookup(userId, media.Id, media.FileName));
                 list.Add(media);
                 recordingsProcessedSoFar++;
 
@@ -70,6 +97,8 @@ namespace QATestRecorder
 
             return await Task.FromResult(list); ;
         }
+
+    
 
         public async Task<IEnumerable<MediaUser>> GetMediaForUsersAsync(MediaForUsersArgs args, DataConnectorProperties properties)
         {
@@ -88,10 +117,12 @@ namespace QATestRecorder
             return result;
         }
 
+       
+
         public async Task<IEnumerable<Media>> GetMediaByIdsAsync(IEnumerable<string> ids, DataConnectorProperties properties)
         {
             var list = new List<Media>();
-
+            
             foreach (var recordingId in ids)
             {
                 var media = new Media
@@ -99,7 +130,7 @@ namespace QATestRecorder
                     Id = recordingId,
                     Date = DateTime.Now.AddDays(-1),
                     Metadata = "{ \"metadata\" : [{\"label\" : \"Claim No\", \"value\" : \"123456\", \"field\" : \"claim_no\", \"type\" : \"number\"},{\"label\" : \"Caller Id\", \"value\" : \"004401232312311\", \"field\" : \"caller_id\", \"type\" : \"number\" }, {\"label\" : \"Account No\", \"value\" : \"12121231314AB\", \"field\" : \"account_no\", \"type\" : \"string\" }, {\"label\" : \"A really long label description\", \"value\" : \"A really long piece of call metadata information 1234567890 1234567890\", \"field\" : \"notes\", \"type\" : \"string\" }] }",
-                    FileName = _defaultFile ?? "a.wmv"
+                    FileName = GetRecordingById(recordingId)
                 };
 
                 list.Add(media);
@@ -148,5 +179,49 @@ namespace QATestRecorder
                 TimeZone = args.TimeZone
             };
         }
+
+        private string ConvertAgentIdToMediaFile(string userId)
+        {
+            string mediaName = GetDefaultFilename();
+            List<string> mediaTypes = new List<string>() { "wmv", "wav", "mp3", "mp4", "avi", "mov" };
+            string[] mediaFileNameLookups = { "a.wmv", "a.wav", "c.mp3", "a.mp4", "b.avi", "b.mov" };
+            string userLookup = string.Empty;
+
+            userLookup = userId.Contains(".") ? userId.Split('.')[1] : userId;
+
+            var index = mediaTypes.FindIndex(x => x.Contains(userLookup));
+
+            if (index > -1)
+            {
+                mediaName = mediaFileNameLookups[index];
+            }
+
+            return mediaName;
+        }
+
+        private string GetRecordingById(string recordingId)
+        {
+            string mediaName = GetDefaultFilename();
+
+            var lookup = _agentRecordingsLookup.FirstOrDefault(x => x.RecordingId == recordingId);
+
+            if (lookup != null)
+            {
+                mediaName = lookup.Filename;
+            }
+
+            return mediaName;
+        }
+
+        private string GetDefaultFilename()
+        {
+            return _defaultFile ?? "a.wmv";
+        }
+
+        private void PurgeOldRecordings()
+        {
+            _agentRecordingsLookup.RemoveAll(x => x.Created < DateTime.Now.AddDays(-1));
+        }
+
     }
 }
